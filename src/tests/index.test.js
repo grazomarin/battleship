45f11336e3fc0214c8a54237ae5ae07970525c36
grazomarin <huseynovk355@gmail.com>
2022-12-30 16:45:05 +0400

    added tests that check if placeShip coordinates are valid

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -19,20 +19,43 @@ describe("Ship", () => {
 describe("Gameboard", () => {
   test("placeShip function", () => {
     const myBoard = new Gameboard();
+    myBoard.init();
     myBoard.placeShip([0, 1], 3);
     expect(myBoard.board[0][1].ship instanceof Ship).toBe(true);
     expect(myBoard.board[0][2].ship instanceof Ship).toBe(true);
     expect(myBoard.board[0][3].ship instanceof Ship).toBe(true);
   });
 
+  test("placeShip to an invalid coordinate", () => {
+    const myBoard = new Gameboard();
+    myBoard.init();
+    expect(() => myBoard.placeShip([0, 8], 3)).toThrow(
+      "ship cannot be placed here"
+    );
+  });
+
+  test("placeShip to a coordinate that already has a ship", () => {
+    const myBoard = new Gameboard();
+    myBoard.init();
+    const fakeShip = new Ship(3);
+    myBoard.board[1][1].ship = fakeShip;
+    myBoard.board[1][2].ship = fakeShip;
+    myBoard.board[1][3].ship = fakeShip;
+    expect(() => {
+      myBoard.placeShip([1, 2], 5);
+    }).toThrow("there is a ship already placed");
+  });
+
   test("receiveAttack to an empty cell", () => {
     const myBoard = new Gameboard();
+    myBoard.init();
     myBoard.receiveAttack([1, 1]);
     expect(myBoard.board[1][1].hit).toBe(true);
   });
 
   test("receiveAttack to a cell that has a ship", () => {
     const myBoard = new Gameboard();
+    myBoard.init();
     const fakeShip = new Ship(3);
     myBoard.board[1][1].ship = fakeShip;
     myBoard.board[1][2].ship = fakeShip;
