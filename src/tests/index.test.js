2737981ffea7f878e5151023d0015770e676cd74
grazomarin <huseynovk355@gmail.com>
2022-12-31 11:39:56 +0400

    added Player class attack func tests

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,11 +1,12 @@
 const { describe, test, expect } = require("@jest/globals");
-import { Ship, Gameboard } from "../index.js";
+import { Ship, Gameboard, Player } from "../index.js";
 
-describe("Ship", () => {
+describe("Ship class", () => {
   test("check hit function", () => {
     const ship = new Ship(2);
     ship.hit();
     expect(ship.hits).toBe(1);
+    expect(ship.isSunk()).toBe(false);
   });
 
   test("check isSunk function", () => {
@@ -16,19 +17,19 @@ describe("Ship", () => {
   });
 });
 
-describe("Gameboard", () => {
+describe("Gameboard class", () => {
   test("placeShip function", () => {
     const myBoard = new Gameboard();
-    myBoard.init();
     myBoard.placeShip([0, 1], 3);
     expect(myBoard.board[0][1].ship instanceof Ship).toBe(true);
     expect(myBoard.board[0][2].ship instanceof Ship).toBe(true);
     expect(myBoard.board[0][3].ship instanceof Ship).toBe(true);
+    expect(myBoard.ships[0] instanceof Ship).toBe(true);
+    expect(myBoard.ships.length).toBe(1);
   });
 
   test("placeShip vertically", () => {
     const myBoard = new Gameboard();
-    myBoard.init();
     myBoard.placeShip([0, 1], 3, true);
     expect(myBoard.board[0][2].ship instanceof Ship).toBe(false);
     expect(myBoard.board[0][3].ship instanceof Ship).toBe(false);
@@ -40,15 +41,11 @@ describe("Gameboard", () => {
 
   test("placeShip to an invalid coordinate", () => {
     const myBoard = new Gameboard();
-    myBoard.init();
-    expect(() => myBoard.placeShip([0, 8], 3)).toThrow(
-      "ship cannot be placed here"
-    );
+    expect(() => myBoard.placeShip([10, 10], 3)).toThrow("invalid coordinates");
   });
 
   test("placeShip to a coordinate that already has a ship", () => {
     const myBoard = new Gameboard();
-    myBoard.init();
     const fakeShip = new Ship(3);
     myBoard.board[1][1].ship = fakeShip;
     myBoard.board[1][2].ship = fakeShip;
@@ -60,14 +57,12 @@ describe("Gameboard", () => {
 
   test("receiveAttack to an empty cell", () => {
     const myBoard = new Gameboard();
-    myBoard.init();
     myBoard.receiveAttack([1, 1]);
     expect(myBoard.board[1][1].hit).toBe(true);
   });
 
   test("receiveAttack to a cell that has a ship", () => {
     const myBoard = new Gameboard();
-    myBoard.init();
     const fakeShip = new Ship(3);
     myBoard.board[1][1].ship = fakeShip;
     myBoard.board[1][2].ship = fakeShip;
@@ -78,4 +73,37 @@ describe("Gameboard", () => {
     expect(myBoard.board[1][2].ship.hits).toBe(1);
     expect(myBoard.board[1][3].ship.hits).toBe(1);
   });
+});
+
+describe("Player class", () => {
+  test("attack a valid empty space", () => {
+    const player1 = new Player();
+    const player2 = new Player();
+    player1.attack([1, 1], player2.gameboard);
+    expect(player2.gameboard.board[1][1].hit).toBe(true);
+  });
+
+  test("attack a valid space with enemy ship", () => {
+    const player1 = new Player();
+    const player2 = new Player();
+    player2.gameboard.placeShip([1, 1], 3);
+    player1.attack([1, 1], player2.gameboard);
+    expect(player2.gameboard.board[1][1].hit).toBe(true);
+    expect(player2.gameboard.board[1][1].ship.hits).toBe(1);
+  });
+
+  test("attack an invalid space", () => {
+    const player1 = new Player();
+    const player2 = new Player();
+
+    expect(() => player1.attack([10, 10], player2.gameboard)).toThrow(
+      "invalid coordinates"
+    );
+  });
+
+  //   test.skip("AI attack", () => {
+  //     const player1 = new Player();
+  //     const player2 = new Player(true);
+  //     player2.randomAttack(player1.gameboard);
+  //   });
 });
