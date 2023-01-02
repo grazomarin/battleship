3bda9b3e2e555b1a53cb891ce3e9770b42f6109e
grazomarin <huseynovk355@gmail.com>
2023-01-02 20:12:09 +0400

    added tests for randomAttack and attack on the same coordinate

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -55,13 +55,13 @@ describe("Gameboard class", () => {
     }).toThrow("there is a ship already placed");
   });
 
-  test("receiveAttack to an empty cell", () => {
+  test("receiveAttack to an empty coordinate", () => {
     const myBoard = new Gameboard();
     myBoard.receiveAttack([1, 1]);
     expect(myBoard.board[1][1].hit).toBe(true);
   });
 
-  test("receiveAttack to a cell that has a ship", () => {
+  test("receiveAttack to a coordinate that has a ship", () => {
     const myBoard = new Gameboard();
     const fakeShip = new Ship(3);
     myBoard.board[1][1].ship = fakeShip;
@@ -76,14 +76,14 @@ describe("Gameboard class", () => {
 });
 
 describe("Player class", () => {
-  test("attack a valid empty space", () => {
+  test("attack a valid empty coordinate", () => {
     const player1 = new Player();
     const player2 = new Player();
     player1.attack([1, 1], player2.gameboard);
     expect(player2.gameboard.board[1][1].hit).toBe(true);
   });
 
-  test("attack a valid space with enemy ship", () => {
+  test("attack a valid coordinate with enemy ship", () => {
     const player1 = new Player();
     const player2 = new Player();
     player2.gameboard.placeShip([1, 1], 3);
@@ -92,7 +92,7 @@ describe("Player class", () => {
     expect(player2.gameboard.board[1][1].ship.hits).toBe(1);
   });
 
-  test("attack an invalid space", () => {
+  test("attack an invalid coordinate", () => {
     const player1 = new Player();
     const player2 = new Player();
 
@@ -101,9 +101,45 @@ describe("Player class", () => {
     );
   });
 
-  //   test.skip("AI attack", () => {
-  //     const player1 = new Player();
-  //     const player2 = new Player(true);
-  //     player2.randomAttack(player1.gameboard);
-  //   });
+  test("attack the same coordinate twice", () => {
+    const player1 = new Player();
+    const player2 = new Player();
+    player1.attack([7, 7], player2.gameboard);
+
+    expect(() => player1.attack([7, 7], player2.gameboard)).toThrow(
+      "already hit"
+    );
+  });
+
+  test("AI attack", () => {
+    const player1 = new Player();
+    const player2 = new Player(true);
+    player2.randomAttack(player1.gameboard);
+
+    const attackedCoord = [];
+
+    for (let i = 0; i < 10; i++) {
+      player1.gameboard.board[i].forEach((square) => {
+        if (square.hit === true) attackedCoord.push(square);
+      });
+    }
+    expect(attackedCoord.length).toBe(1);
+  });
+
+  test("AI attacking all coordinates on the board", () => {
+    const player1 = new Player();
+    const player2 = new Player(true);
+    for (let i = 0; i < 100; i++) {
+      player2.randomAttack(player1.gameboard);
+    }
+
+    const attackedCoord = [];
+
+    for (let i = 0; i < 10; i++) {
+      player1.gameboard.board[i].forEach((square) => {
+        if (square.hit === true) attackedCoord.push(square);
+      });
+    }
+    expect(attackedCoord.length).toBe(100);
+  });
 });
