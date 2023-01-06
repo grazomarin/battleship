24dfc9f629c0754eedd0a818be87fa5a86df2e53
grazomarin <huseynovk355@gmail.com>
2023-01-06 14:42:16 +0400

    added reserveAround function
    space around a ship is reserved and no ship can be placed there

--- src/script_modules/game.js
+++ src/script_modules/game.js
@@ -5,8 +5,7 @@ function initGame() {
   const p1 = new Player();
   const AI = new Player(true);
   p1.gameboard.randomFleet();
-  AI.gameboard.placeShip([5, 6], 3, true);
-  AI.gameboard.placeShip([3, 0], 5, false);
+  AI.gameboard.randomFleet();
   renderBoards(p1, AI);
 }
 
--- src/script_modules/gameboard.js
+++ src/script_modules/gameboard.js
@@ -11,6 +11,7 @@ class Gameboard {
         row.push({
           hit: false,
           ship: false,
+          reserved: false,
         });
       }
       this.board.push(row);
@@ -43,6 +44,32 @@ class Gameboard {
     return this.board[c[0]][c[1]].hit ? true : false;
   }
 
+  isReserved(c) {
+    return this.board[c[0]][c[1]].reserved === true ? true : false;
+  }
+
+  #reserveSpace(c) {
+    const y = c[0];
+    const x = c[1];
+    // reserves space around a coordinate
+    if (this.isValidCoordinate([y + 1, x]))
+      this.board[y + 1][x].reserved = true;
+    if (this.isValidCoordinate([y + 1, x + 1]))
+      this.board[y + 1][x + 1].reserved = true;
+    if (this.isValidCoordinate([y, x + 1]))
+      this.board[y][x + 1].reserved = true;
+    if (this.isValidCoordinate([y - 1, x + 1]))
+      this.board[y - 1][x + 1].reserved = true;
+    if (this.isValidCoordinate([y - 1, x]))
+      this.board[y - 1][x].reserved = true;
+    if (this.isValidCoordinate([y - 1, x - 1]))
+      this.board[y - 1][x - 1].reserved = true;
+    if (this.isValidCoordinate([y, x - 1]))
+      this.board[y][x - 1].reserved = true;
+    if (this.isValidCoordinate([y + 1, x - 1]))
+      this.board[y + 1][x - 1].reserved = true;
+  }
+
   returnRandomCoordinates() {
     const y = Math.floor(Math.random() * 10);
     const x = Math.floor(Math.random() * 10);
@@ -58,17 +85,26 @@ class Gameboard {
       if (!this.isValidCoordinate([y + (ship.length - 1), x])) return false;
       for (let i = 0; i < length; i++) {
         if (this.hasShip([y + i, x])) return false;
+        if (this.isReserved([y + i, x])) return false;
       }
       for (let i = 0; i < length; i++) {
         this.board[y + i][x].ship = ship;
+      }
+
+      for (let i = 0; i < length; i++) {
+        this.#reserveSpace([y + i, x]);
       }
     } else {
       if (!this.isValidCoordinate([y, x + (ship.length - 1)])) return false;
       for (let i = 0; i < length; i++) {
         if (this.hasShip([y, x + i])) return false;
+        if (this.isReserved([y, x + i])) return false;
       }
       for (let i = 0; i < length; i++) {
         this.board[y][x + i].ship = ship;
+      }
+      for (let i = 0; i < length; i++) {
+        this.#reserveSpace([y, x + i]);
       }
     }
     this.ships.push(ship);
@@ -98,8 +134,8 @@ class Gameboard {
       }
       u++;
     }
-    //place 1 ship length of 5
-    for (let z = 0; z <= 1; z++) {
+    //place 2 ships length of 5
+    for (let z = 0; z <= 2; z++) {
       let [y, x] = this.returnRandomCoordinates();
       while (this.placeShip([y, x], 5, !!Math.round(Math.random())) === false) {
         [y, x] = this.returnRandomCoordinates();
